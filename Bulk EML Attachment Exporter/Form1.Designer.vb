0c1993039fe19947721b4b501300568685363fdc
Alex Cortinovis <walker1993@live.it>
2018-02-20 15:55:51 +0100

    Primo push

---
+++ Bulk EML Attachment Exporter/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Apri eml"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.DefaultExt = "eml"
+        Me.OpenFileDialog1.Filter = "EML|*.eml"
+        Me.OpenFileDialog1.Multiselect = True
+        Me.OpenFileDialog1.RestoreDirectory = True
+        Me.OpenFileDialog1.SupportMultiDottedExtensions = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(93, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(105, 23)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "Salva allegati"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(552, 309)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Bulk EML Attachment Exporter/Form1.vb
@@ -0,0 +1,30 @@
+﻿
+Imports LumiSoft.Net.Mime
+
+Public Class Form1
+    Dim files As New List(Of String)
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dialog = OpenFileDialog1.ShowDialog
+        If dialog = DialogResult.OK Then
+            files.AddRange(OpenFileDialog1.FileNames)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim message As Mime
+        For Each file In files
+            message = Mime.Parse(file)
+            Dim info As New IO.FileInfo(file)
+            Dim savedir As IO.DirectoryInfo
+            If Not IO.Directory.Exists(info.DirectoryName + "\" + file) Then
+                savedir = IO.Directory.CreateDirectory(info.DirectoryName + "\" + info.Name.Substring(0, info.Name.LastIndexOf(".eml")))
+            Else
+                savedir = New IO.DirectoryInfo(info.DirectoryName + "\" + file)
+            End If
+            For Each att As MimeEntity In message.Attachments
+                IO.File.WriteAllBytes(savedir.FullName & "\" & If(att.ContentDisposition_FileName, att.ContentType_Name), att.Data)
+            Next
+        Next
+    End Sub
+End Class
